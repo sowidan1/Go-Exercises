44a6988ad3c095ae9ca81eb36c2e0416abf25277
sowidan1 <osamasowidan1@gmail.com>
2024-03-03 23:48:07 +0200

    Upload some exm

---
+++ input.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+func main(){
+	var fname string
+	fmt.Println("enter frist name")
+	fmt.Scan(&fname)
+
+	var lname string
+	fmt.Println("enter frist name")
+	fmt.Scan(&lname)
+
+	fmt.Println("kffk" + "p2pffp"+fname)
+
+	fmt.Println("ff")
+}
